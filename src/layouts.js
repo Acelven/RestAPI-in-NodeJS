98bc78850eeeef1cd20a1af1df72b3ec954356d1
Acelven <124097190+Acelven@users.noreply.github.com>
2023-10-24 23:41:30 +0000

    Fix

--- src/layouts.js
+++ src/layouts.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './navbar.js';
 import Tracking from './tracking.js';
 import Login from './login.js'
 
 const Layouts = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Navbar /> {/* Navbar always stays on top */}
       <Routes>
         <Route path="/tracking" element={<Tracking />} />
