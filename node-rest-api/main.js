71080683953b437af73c8bf30e6b146590fb509d
Acelven <124097190+Acelven@users.noreply.github.com>
2023-10-24 20:24:16 +0000

    fix

--- node-rest-api/main.js
+++ node-rest-api/main.js
@@ -5,9 +5,6 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 const secretKey = 'your-secret-key';
 
 app.use(express.json());
